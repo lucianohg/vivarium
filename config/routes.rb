05c751d736d015b39adf61cf5fc49763b5e11eef
Maestro <luciano.hg4@gmail.com>
2013-09-28 12:56:44 -0300

    Add A StaticPages Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Vivarium::Application.routes.draw do
+  root 'static_pages#home'
+  match '/team', to: 'static_pages#team' via: 'get'
+  match '/clients', to: 'static_pages#clients' via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET 'home'" do
+    it "returns http success" do
+      get 'home'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'team'" do
+    it "returns http success" do
+      get 'team'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'clients'" do
+    it "returns http success" do
+      get 'clients'
+      response.should be_success
+    end
+  end
+
+end
